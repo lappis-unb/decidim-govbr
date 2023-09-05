84c9401b8da037899bdc4042bc67b4f7e43e3584
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-09-05 17:58:21 -0300

    feat: add index page to user statistics

--- config/initializers/decidim_govbr_user_proposals_statistic.rb
+++ config/initializers/decidim_govbr_user_proposals_statistic.rb
@@ -2,7 +2,18 @@
 Decidim.menu :admin_user_menu do |menu|
   menu.add_item :export_users_statistic,
                 'Relatório de estatísticas de usuários',
-                'user_proposal_statistic',
+                'admin/user_proposal_statistics',
                 active: is_active_link?('user_proposal_statistic'),
                 if: allowed_to?(:read, :admin_user)
+end
+
+# Add link to export user statistics in Decidim Admin Menu
+Decidim.menu :admin_menu do |menu|
+  menu.add_item :users_proposals_statistics,
+                'Relatório de estatísticas de usuários',
+                'admin/user_proposal_statistics',
+                active: is_active_link?('user_proposal_statistics'),
+                if: allowed_to?(:read, :admin_user),
+                icon_name: 'chat',
+                position: 5
 end
