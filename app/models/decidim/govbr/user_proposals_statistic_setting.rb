23da28c6ca76b38b2d9cc97dcbd4e81194e78361
VictorJorgeFGA <victor.eng.unb@gmail.com>
2023-09-04 23:01:46 -0300

    feat: add refresh function

--- app/models/decidim/govbr/user_proposals_statistic_setting.rb
+++ app/models/decidim/govbr/user_proposals_statistic_setting.rb
@@ -8,50 +8,114 @@ module Decidim
 
       has_many :user_proposals_statistics, class_name: 'Decidim::Govbr::UserProposalsStatistic'
 
-      def proposals
-        @proposals ||= Decidim::Proposals::Proposal.where(
-          component: Decidim::Component.where(
-            participatory_space_id: decidim_participatory_space_id,
-            participatory_space_type: decidim_participatory_space_type
-          )
-        )
-      end
-
-      def users
-        @users ||= Decidim::User.joins(:coauthorships).
-          where(coauthorships: {
-              coauthorable: proposals
-            }
-          )
-      end
-
-      # Rebuild entire table from scratch with updated decidim database content for
-      # the specified participatory_space
+      # Rebuild entire table from scratch with updated decidim database content for the specified participatory_space
       def refresh_data
         user_proposals_statistics.delete_all
 
-        refresh_user_proposals_authorship_data
-        refresh_user_comments_data
-        refresh_user_votes_data
-        refresh_user_follows_data
-      end
+        statistic_data = {}
 
-      def refresh_user_proposals_authorship_data
-        Decidim::User.find_each do |u|
-          user_proposals_statistics.create(decidim_user_id: u.id)
+        get_user_proposals_data.to_a.each { |data|
+          statistic_data[data['decidim_user_id']] = data
+        }
+
+        get_user_comments_data.to_a.each { |data|
+          statistic_data[data['decidim_user_id']] = (statistic_data[data['decidim_user_id']] || {}).merge(data)
+        }
+
+        get_user_votes_data.to_a.each { |data|
+          statistic_data[data['decidim_user_id']] = (statistic_data[data['decidim_user_id']] || {}).merge(data)
+        }
+
+        get_user_follows_data.to_a.each { |data|
+          statistic_data[data['decidim_user_id']] = (statistic_data[data['decidim_user_id']] || {}).merge(data)
+        }
+
+        statistic_data.each do |user_id, data|
+          data['proposals_done'] = data['proposals_done'].to_f * proposals_done_weight
+          data['votes_done'] = data['votes_done'].to_f * votes_done_weight
+          data['comments_received'] = data['comments_received'].to_f * comments_received_weight
+          data['follows_received'] = data['follows_received'].to_f * follows_received_weight
+          data['comments_done'] = data['comments_done'].to_f * comments_done_weight
+          data['votes_done'] = data['votes_done'].to_f * votes_done_weight
+          data['follows_done'] = data['follows_done'].to_f * follows_done_weight
+          data['user_proposals_statistic_setting_id'] = id
         end
+
+        Decidim::Govbr::UserProposalsStatistic.insert_all(statistic_data.values)
       end
 
-      def refresh_user_comments_data
-
+      def get_user_proposals_data
+        ActiveRecord::Base.connection.execute(
+          <<~SQL.squish
+          SELECT du.id as decidim_user_id, du.name as decidim_user_name, COUNT(dpp.id) as proposals_done, SUM(dpp.proposal_votes_count) as votes_received, SUM(dpp.comments_count) as comments_received, SUM(dpp.follows_count) as follows_received
+            FROM decidim_components as dc
+            INNER JOIN decidim_proposals_proposals as dpp
+              ON dc.id = dpp.decidim_component_id
+            INNER JOIN decidim_coauthorships as coauth
+              ON coauth.coauthorable_type = 'Decidim::Proposals::Proposal' AND coauth.coauthorable_id = dpp.id
+            INNER JOIN decidim_users as du
+              ON du.id = coauth.decidim_author_id
+            WHERE dc.participatory_space_type = '#{decidim_participatory_space_type}'
+              AND dc.participatory_space_id = '#{decidim_participatory_space_id}'
+          GROUP BY du.id
+          SQL
+        )
       end
 
-      def refresh_user_votes_data
-
+      def get_user_comments_data
+        ActiveRecord::Base.connection.execute(
+          <<~SQL.squish
+          SELECT du.id as decidim_user_id, COUNT(dcc.id) as comments_done
+            FROM decidim_components as dc
+            INNER JOIN decidim_proposals_proposals as dpp
+              ON dc.id = dpp.decidim_component_id
+            INNER JOIN decidim_comments_comments as dcc
+              ON dcc.decidim_commentable_id = dpp.id
+            INNER JOIN decidim_users as du
+              ON du.id = dcc.decidim_author_id
+            WHERE dcc.decidim_commentable_type = 'Decidim::Proposals::Proposal'
+              AND dc.participatory_space_type = '#{decidim_participatory_space_type}'
+              AND dc.participatory_space_id = '#{decidim_participatory_space_id}'
+          GROUP BY du.id
+          SQL
+        )
       end
 
-      def refresh_user_follows_data
+      def get_user_votes_data
+        ActiveRecord::Base.connection.execute(
+          <<~SQL.squish
+          SELECT du.id as decidim_user_id, COUNT(dppv.id) as votes_done
+            FROM decidim_components as dc
+            INNER JOIN decidim_proposals_proposals as dpp
+              ON dc.id = dpp.decidim_component_id
+            INNER JOIN decidim_proposals_proposal_votes as dppv
+              ON dppv.decidim_proposal_id = dpp.id
+            INNER JOIN decidim_users as du
+              ON du.id = dppv.decidim_author_id
+            WHERE dc.participatory_space_type = '#{decidim_participatory_space_type}'
+              AND dc.participatory_space_id = '#{decidim_participatory_space_id}'
+          GROUP BY du.id
+          SQL
+        )
+      end
 
+      def get_user_follows_data
+        ActiveRecord::Base.connection.execute(
+          <<~SQL.squish
+          SELECT du.id as decidim_user_id, COUNT(df.id) as follows_done
+            FROM decidim_components as dc
+            INNER JOIN decidim_proposals_proposals as dpp
+              ON dc.id = dpp.decidim_component_id
+            INNER JOIN decidim_follows as df
+              ON df.decidim_followable_id = dpp.id
+            INNER JOIN decidim_users as du
+              ON du.id = df.decidim_user_id
+            WHERE df.decidim_followable_type = 'Decidim::Proposals::Proposal'
+              AND dc.participatory_space_type = '#{decidim_participatory_space_type}'
+              AND dc.participatory_space_id = '#{decidim_participatory_space_id}'
+          GROUP BY du.id
+          SQL
+        )
       end
     end
   end
