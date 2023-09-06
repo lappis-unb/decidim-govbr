d46a2ed2787a447a66321bbb59985402dd6331c8
VictorJorgeFGA <victor.eng.unb@gmail.com>
2023-09-06 00:16:22 -0300

    refactor: does some refactor

--- app/models/decidim/govbr/user_proposals_statistic.rb
+++ app/models/decidim/govbr/user_proposals_statistic.rb
@@ -12,19 +12,19 @@ module Decidim
       belongs_to :user_proposals_statistic_setting, class_name: 'Decidim::Govbr::UserProposalsStatisticSetting'
 
       def self.csv_attributes_header_map
-        [
-          ['decidim_user_id', 'ID do Usuário'],
-          ['decidim_user_identification_number', 'CPF'],
-          ['decidim_user_name', 'Nome'],
-          ['proposals_done', 'Propostas criadas'],
-          ['comments_done', 'Comentários feitos em propostas'],
-          ['votes_done', 'Votos dados em propostas'],
-          ['follows_done', 'Propostas que segue'],
-          ['votes_received', 'Votos recebidos em suas propostas'],
-          ['comments_received', 'Comentários recebidos em suas propostas'],
-          ['follows_received', 'Seguidores em suas propostas'],
-          ['score', 'Pontuação total']
-        ]
+        {
+          'decidim_user_id' => 'ID do Usuário',
+          'decidim_user_name' => 'Nome',
+          'decidim_user_identification_number' => 'CPF',
+          'proposals_done' => 'Propostas criadas',
+          'comments_done' => 'Comentários feitos em propostas',
+          'votes_done' => 'Votos dados em propostas',
+          'follows_done' => 'Propostas que segue',
+          'votes_received' => 'Votos recebidos em suas propostas',
+          'comments_received' => 'Comentários recebidos em suas propostas',
+          'follows_received' => 'Seguidores em suas propostas',
+          'score' => 'Pontuação total'
+        }
       end
     end
   end
