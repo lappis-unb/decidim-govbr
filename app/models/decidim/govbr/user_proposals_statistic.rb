01bf5cc4469cc91cafcb9c7bbd32d2683f07aff9
VictorJorgeFGA <victor.eng.unb@gmail.com>
2023-09-05 22:42:57 -0300

    feat: add default setting and add ability to export in csv format

--- app/models/decidim/govbr/user_proposals_statistic.rb
+++ app/models/decidim/govbr/user_proposals_statistic.rb
@@ -10,6 +10,22 @@ module Decidim
       self.table_name = 'decidim_govbr_user_proposals_statistics'
 
       belongs_to :user_proposals_statistic_setting, class_name: 'Decidim::Govbr::UserProposalsStatisticSetting'
+
+      def self.csv_attributes_header_map
+        [
+          ['decidim_user_id', 'ID do Usuário'],
+          ['decidim_user_identification_number', 'CPF'],
+          ['decidim_user_name', 'Nome'],
+          ['proposals_done', 'Propostas criadas'],
+          ['comments_done', 'Comentários feitos em propostas'],
+          ['votes_done', 'Votos dados em propostas'],
+          ['follows_done', 'Propostas que segue'],
+          ['votes_received', 'Votos recebidos em suas propostas'],
+          ['comments_received', 'Comentários recebidos em suas propostas'],
+          ['follows_received', 'Seguidores em suas propostas'],
+          ['score', 'Pontuação total']
+        ]
+      end
     end
   end
 end
--- config/initializers/decidim_govbr_user_proposals_statistic.rb
+++ config/initializers/decidim_govbr_user_proposals_statistic.rb
@@ -1,19 +1,14 @@
-# Add link to export user statistics in Decidim Admin Menu
-Decidim.menu :admin_user_menu do |menu|
-  menu.add_item :export_users_statistic,
-                'Relatório de estatísticas de usuários',
-                'admin/user_proposal_statistics',
-                active: is_active_link?('user_proposal_statistic'),
-                if: allowed_to?(:read, :admin_user)
-end
-
-# Add link to export user statistics in Decidim Admin Menu
-Decidim.menu :admin_menu do |menu|
-  menu.add_item :users_proposals_statistics,
-                'Relatório de estatísticas de usuários',
-                'admin/user_proposal_statistics',
-                active: is_active_link?('user_proposal_statistics'),
-                if: allowed_to?(:read, :admin_user),
-                icon_name: 'chat',
-                position: 5
+begin
+  # Add link to export user statistics in Decidim Admin Menu
+  Decidim.menu :admin_menu do |menu|
+    menu.add_item :users_proposals_statistics,
+    'Relatório de estatísticas de usuários ENDG',
+    '/admin/user_proposal_statistic_report/engd',
+    active: is_active_link?('user_proposal_statistics'),
+    if: allowed_to?(:read, :admin_user),
+    icon_name: 'chat',
+    position: 5
+  end
+rescue
+  # Ensure APP initializes even if this task breaks
 end
