b24959d515ff44ba8441af2702f0038118c2b086
VictorJorgeFGA <victor.eng.unb@gmail.com>
2023-09-09 19:32:47 -0300

    fix: fixes initializer to add menu item for conjuv

--- config/initializers/decidim_govbr_user_proposals_statistic.rb
+++ config/initializers/decidim_govbr_user_proposals_statistic.rb
@@ -2,12 +2,12 @@ begin
   # Add link to export user statistics in Decidim Admin Menu
   Decidim.menu :admin_menu do |menu|
     menu.add_item :users_proposals_statistics,
-    'Relatório de estatísticas de usuários ENDG',
-    '/admin/user_proposal_statistic_report/engd',
+    'Relatório de estatísticas de usuários Conf. da Juventude',
+    '/admin/user_proposal_statistic_report/juventude',
     active: is_active_link?('user_proposal_statistics'),
     if: allowed_to?(:read, :admin_user),
-    icon_name: 'chat',
-    position: 5
+    icon_name: 'envelope-closed',
+    position: 4.6
   end
 rescue
   # Ensure APP initializes even if this task breaks
