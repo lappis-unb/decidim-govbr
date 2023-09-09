b1f2eb8deb05dd017ef46e553fc6fa473f19d5bc
VictorJorgeFGA <victor.eng.unb@gmail.com>
2023-09-09 19:32:25 -0300

    fix: fixes migrations to cover conjuv case

--- db/migrate/20230820015020_create_decidim_govbr_user_proposals_statistics.rb
+++ db/migrate/20230820015020_create_decidim_govbr_user_proposals_statistics.rb
@@ -1,6 +1,5 @@
 class CreateDecidimGovbrUserProposalsStatistics < ActiveRecord::Migration[6.1]
   def change
-    # TODO: adicionar default de 0 nos fields
     create_table :decidim_govbr_user_proposals_statistics do |t|
       t.bigint :decidim_user_id, null: false
       t.string :decidim_user_identification_number, null: false, default: ''
@@ -18,18 +17,5 @@ class CreateDecidimGovbrUserProposalsStatistics < ActiveRecord::Migration[6.1]
 
     add_reference :decidim_govbr_user_proposals_statistics, :user_proposals_statistic_setting, index: { name: :user_proposals_statistics_on_settings_idx }
     add_index :decidim_govbr_user_proposals_statistics, :decidim_user_id, name: :decidim_govbr_user_proposals_statistic_user_idx
-
-    begin
-      statistic_setting = Decidim::Govbr::UserProposalsStatisticSetting.find_by(decidim_participatory_space_id: Decidim::ParticipatoryProcess.find_by(slug: 'engd').id)
-      unless statistic_setting.present?
-        Decidim::Govbr::UserProposalsStatisticSetting.create(
-          decidim_participatory_space_id: Decidim::ParticipatoryProcess.find_by(slug: 'engd').id,
-          decidim_participatory_space_type: 'Decidim::ParticipatoryProcess',
-          name: 'Relatorio de atividade Usuario x Propostas - ENGD'
-        )
-      end
-    rescue
-      # Ensure migration runs regardless of this task
-    end
   end
 end
