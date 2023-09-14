84771784e4a348a94c30ad721d5d973dfe772ddd
VictorJorgeFGA <victor.eng.unb@gmail.com>
2023-09-13 23:27:30 -0300

    test: enhance test cases coverage

--- test/models/decidim/govbr/user_proposals_statistic_setting_test.rb
+++ test/models/decidim/govbr/user_proposals_statistic_setting_test.rb
@@ -162,6 +162,64 @@ module Decidim
             comment.save!(validate: false)
           end
         end
+
+        @user4 = Decidim::User.new(
+          email: "user4@example.com",
+          name: "User 4",
+          nickname: "user_4",
+          password: "OMX*&##*UNIASDLLPOASPMBASE64",
+          password_confirmation: "OMX*&##*UNIASDLLPOASPMBASE64",
+          confirmed_at: Time.current,
+          decidim_organization_id: @org.id,
+          type: "Decidim::User",
+        )
+        @user4.save!(validate: false)
+
+        @user5 = Decidim::User.new(
+          email: "user5@example.com",
+          name: "User 5",
+          nickname: "user_5",
+          password: "OMX*&##*UNIASDLLPOASPMBASE64",
+          password_confirmation: "OMX*&##*UNIASDLLPOASPMBASE64",
+          confirmed_at: Time.current,
+          decidim_organization_id: @org.id,
+          type: "Decidim::User",
+        )
+        @user5.save!(validate: false)
+
+        @user6 = Decidim::User.new(
+          email: "user6@example.com",
+          name: "User 6",
+          nickname: "user_6",
+          password: "OMX*&##*UNIASDLLPOASPMBASE64",
+          password_confirmation: "OMX*&##*UNIASDLLPOASPMBASE64",
+          confirmed_at: Time.current,
+          decidim_organization_id: @org.id,
+          type: "Decidim::User",
+        )
+        @user6.save!(validate: false)
+
+        Decidim::Proposals::Proposal.find_each do |proposal|
+          vote = Decidim::Proposals::ProposalVote.new(
+            proposal: proposal,
+            author: @user4
+          )
+          vote.save!(validate: false)
+
+          comment = Decidim::Comments::Comment.new(
+            root_commentable: proposal,
+            commentable: proposal,
+            author: @user5,
+            body: {"pt" => "this is a comment"}
+          )
+          comment.save!(validate: false)
+
+          follow = Decidim::Follow.new(
+            user: @user6,
+            followable: proposal
+          )
+          follow.save!(validate: false)
+        end
       end
 
       test 'should only consider proposals data from the registered participatory space' do
@@ -171,41 +229,83 @@ module Decidim
           name: 'relatorio'
         )
         assert_equal 4, Decidim::Proposals::Proposal.count
-        assert_equal 12, Decidim::Comments::Comment.count
-        assert_equal 4, Decidim::Follow.count
+        assert_equal 16, Decidim::Comments::Comment.count
+        assert_equal 8, Decidim::Follow.count
 
         setting.refresh_data!
         setting.reload
 
         statistic = setting.user_proposals_statistics.where(decidim_user_id: @user1.id).first
+        assert_equal @user1.name, statistic.decidim_user_name
         assert_equal 2, statistic.proposals_done
         assert_equal 3, statistic.comments_done
         assert_equal 3, statistic.votes_done
         assert_equal 2, statistic.follows_done
-        assert_equal 6, statistic.votes_received
-        assert_equal 6, statistic.comments_received
-        assert_equal 2, statistic.follows_received
+        assert_equal 8, statistic.votes_received
+        assert_equal 8, statistic.comments_received
+        assert_equal 4, statistic.follows_received
 
         expected_score = statistic.proposals_done + statistic.comments_done + statistic.votes_done + statistic.follows_done + statistic.votes_received + statistic.comments_received + statistic.follows_received
         assert_equal expected_score.to_f, statistic.score
 
         statistic = setting.user_proposals_statistics.where(decidim_user_id: @user2.id).first
+        assert_equal @user2.name, statistic.decidim_user_name
         assert_equal 1, statistic.proposals_done
         assert_equal 3, statistic.comments_done
         assert_equal 3, statistic.votes_done
         assert_equal 1, statistic.follows_done
-        assert_equal 3, statistic.votes_received
-        assert_equal 3, statistic.comments_received
-        assert_equal 1, statistic.follows_received
+        assert_equal 4, statistic.votes_received
+        assert_equal 4, statistic.comments_received
+        assert_equal 2, statistic.follows_received
 
         expected_score = statistic.proposals_done + statistic.comments_done + statistic.votes_done + statistic.follows_done + statistic.votes_received + statistic.comments_received + statistic.follows_received
         assert_equal expected_score.to_f, statistic.score
 
         statistic = setting.user_proposals_statistics.where(decidim_user_id: @user3.id).first
+        assert_equal @user3.name, statistic.decidim_user_name
         assert_equal 0, statistic.proposals_done
         assert_equal 3, statistic.comments_done
         assert_equal 3, statistic.votes_done
         assert_equal 0, statistic.follows_done
+        assert_equal 0, statistic.votes_received
+        assert_equal 0, statistic.comments_received
+        assert_equal 0, statistic.follows_received
+
+        expected_score = statistic.proposals_done + statistic.comments_done + statistic.votes_done + statistic.follows_done + statistic.votes_received + statistic.comments_received + statistic.follows_received
+        assert_equal expected_score.to_f, statistic.score
+
+        statistic = setting.user_proposals_statistics.where(decidim_user_id: @user4.id).first
+        assert_equal @user4.name, statistic.decidim_user_name
+        assert_equal 0, statistic.proposals_done
+        assert_equal 0, statistic.comments_done
+        assert_equal 3, statistic.votes_done
+        assert_equal 0, statistic.follows_done
+        assert_equal 0, statistic.votes_received
+        assert_equal 0, statistic.comments_received
+        assert_equal 0, statistic.follows_received
+
+        expected_score = statistic.proposals_done + statistic.comments_done + statistic.votes_done + statistic.follows_done + statistic.votes_received + statistic.comments_received + statistic.follows_received
+        assert_equal expected_score.to_f, statistic.score
+
+        statistic = setting.user_proposals_statistics.where(decidim_user_id: @user5.id).first
+        assert_equal @user5.name, statistic.decidim_user_name
+        assert_equal 0, statistic.proposals_done
+        assert_equal 3, statistic.comments_done
+        assert_equal 0, statistic.votes_done
+        assert_equal 0, statistic.follows_done
+        assert_equal 0, statistic.votes_received
+        assert_equal 0, statistic.comments_received
+        assert_equal 0, statistic.follows_received
+
+        expected_score = statistic.proposals_done + statistic.comments_done + statistic.votes_done + statistic.follows_done + statistic.votes_received + statistic.comments_received + statistic.follows_received
+        assert_equal expected_score.to_f, statistic.score
+
+        statistic = setting.user_proposals_statistics.where(decidim_user_id: @user6.id).first
+        assert_equal @user6.name, statistic.decidim_user_name
+        assert_equal 0, statistic.proposals_done
+        assert_equal 0, statistic.comments_done
+        assert_equal 0, statistic.votes_done
+        assert_equal 3, statistic.follows_done
         assert_equal 0, statistic.votes_received
         assert_equal 0, statistic.comments_received
         assert_equal 0, statistic.follows_received
