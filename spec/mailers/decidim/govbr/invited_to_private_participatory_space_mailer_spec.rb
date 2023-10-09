e3bdcd3de2703afa2494cba304775fc02af34af8
Victor Gonçalves <victor.eng.unb@gmail.com>
2023-10-09 17:59:56 -0300

    test: enhance tests

--- spec/mailers/decidim/govbr/invited_to_private_participatory_space_mailer_spec.rb
+++ spec/mailers/decidim/govbr/invited_to_private_participatory_space_mailer_spec.rb
@@ -13,8 +13,9 @@ module Decidim::Govbr
       context 'when it is called with a valid user and participatory space' do
         it 'sends an email containing information about the participatory space' do
           I18n.with_locale(:'pt-BR') do
-            participatory_process.title[:'pt-BR'] = 'Organizacao de Teste'
-            expect(email_body(mail)).to have_tag('p', :text => I18n.t('decidim.govbr.invited_to_private_participatory_space_mailer.notification.greetings', user: user.name))
+            participatory_space_name = participatory_process.title[:'pt-BR'] = 'Organizacao de Teste'
+            expect(email_body(mail)).to have_tag('p', :seen => "Olá, #{user.name}!")
+            expect(email_body(mail)).to have_tag('p', :seen => "Informamos que você foi adicionado(a) como participante privado no espaço participativo #{participatory_space_name}. Este é um e-mail automático e não deve ser respondido.")
           end
         end
       end
