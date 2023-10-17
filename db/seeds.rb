f244c80521a9d8f1f3043f73c17b65b22bb40081
viniciusroriz <fowardshift@gmail.com>
2023-10-17 19:14:46 -0300

    cria organização e muda as cores

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# You can remove the 'faker' gem if you don't want Decidim seeds.
-# Decidim.seed!
+colors = {
+    primary: "#1351b4",
+    secondary: "#2378c3",
+    success: "#168821",
+    warning: "#2670e8",
+    alert: "#e5220a",
+    highlight: "#be6400",
+    theme: "#1351b4"
+}
 
-existing_admin = Decidim::User.find_by(email: ENV['ADMIN_EMAIL'])
+# Cria a organização Brasil Participativo
+organization = Decidim::Organization.find_by(name: 'Brasil Participativo')
 
-if existing_admin
-    puts "Usuário com o e-mail #{ENV['ADMIN_EMAIL']} já existe."
-else
-    Decidim::System::Admin.new(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD']).save!(validate: false)
-    puts "Usuário com o e-mail #{ENV['ADMIN_EMAIL']} criado."
-end
-
-organization = Decidim::Organization.find_by(host: 'localhost')
-
-if organization
-    puts "Já existe uma organização utilizando 'localhost'."
-else
-    brasil_participativo = Decidim::Organization.create!(
+if !organization
+    organization = Decidim::Organization.create!(
         name: 'Brasil Participativo',
         host: 'localhost',
-        default_locale: 'pt-BR',
-        available_locales: Decidim.available_locales,
+        default_locale: 'pt',
+        available_locales: ['en', 'pt'],
         reference_prefix: 'brasil_participativo',
         available_authorizations: Decidim.authorization_workflows.map(&:name),
         users_registration_mode: :enabled,
@@ -35,7 +25,40 @@ else
         user_groups_enabled: true,
         send_welcome_notification: true,
         file_upload_settings: Decidim::OrganizationSettings.default(:upload),
+        external_domain_whitelist: ["decidim.org", "github.com"],
+        colors: colors,
     )
     brasil_participativo.save!
     puts "Organização 'Brasil Participativo' criada."
+# else
+#     organization.update!(
+#         {
+#             colors: colors,
+#             default_locale: 'pt-BR',
+#             available_locales: ['en', 'pt-BR']
+#         }
+#     )
 end
+
+# Cria um admin e relaciona com a organização 
+admin = Decidim::User.find_or_initialize_by(email: 'brasil@participativo.com')
+puts "Usuário com o e-mail brasil@participativo criado ou encontrado."
+
+admin_hash = {
+    name: 'Admin Brasil Participativo',
+    nickname: 'admin_bp',
+    organization: organization,
+    confirmed_at: Time.current,
+    locale: 'pt-BR',
+    admin: true,
+    tos_agreement: true,
+    about: 'Admin do decidim',
+    accepted_tos_version: organization.tos_version + 1.hour,
+    newsletter_notifications_at: Time.current,
+    password_updated_at: Time.current,
+    admin_terms_accepted_at: Time.current
+}
+admin.update!(admin_hash)
+admin.password = 'admin'
+admin.password_confirmation = 'admin'
+admin.save!(validate: false)
