fa85ba6eb7c2d810b1dbe0fdc218b2823d3a616b
oo7gabriel <gabrielfsacademico@gmail.com>
2023-10-18 09:53:23 -0300

    Preparação de arquivos de processos e snippets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Define cores padrões do Brasil Participativo
 colors = {
     primary: "#1351b4",
     secondary: "#2378c3",
@@ -7,6 +8,20 @@ colors = {
     highlight: "#be6400",
     theme: "#1351b4"
 }
+
+# Snippets do Cabeçalho
+
+# arquivo = Rails.root.join(__dir__, 'seeds', 'snippet.html')
+
+# Verifique se o arquivo existe
+# if File.exist?(arquivo)
+#   header_snippets = File.read(arquivo)
+
+#   puts "Header Snnipet criado com sucesso"
+# else
+#   header_snippets = "Arquivo HTML não encontrado"
+#   puts "Header Snnipet não foi criado"
+# end
 
 # Cria a organização Brasil Participativo
 organization = Decidim::Organization.find_by(name: 'Brasil Participativo')
@@ -27,6 +42,7 @@ if !organization
         file_upload_settings: Decidim::OrganizationSettings.default(:upload),
         external_domain_whitelist: ["decidim.org", "github.com"],
         colors: colors,
+    
     )
     brasil_participativo.save!
     puts "Organização 'Brasil Participativo' criada."
@@ -58,7 +74,34 @@ admin_hash = {
     password_updated_at: Time.current,
     admin_terms_accepted_at: Time.current
 }
+
+admin_hash.merge!(password: "decidim123456789") if admin.encrypted_password.blank?
 admin.update!(admin_hash)
-admin.password = 'admin'
-admin.password_confirmation = 'admin'
-admin.save!(validate: false)
+puts "Processo de administrador do sistema concluído com sucesso"
+
+# Criando Blocos de Conteúdo ativo da organização
+
+# Decidim::System::CreateDefaultContentBlocks.call(organization)
+
+# html_content_block = Decidim::ContentBlock.find_by(organization: organization, manifest_name: :html, scope_name: :homepage)
+
+# # Snippets da home page
+
+# home_arquivo = Rails.root.join(__dir__, 'seeds', 'home-page.html')
+
+# # Verifique se o arquivo existe
+# if File.exist?(home_arquivo)
+#   home_snippets = File.read(home_arquivo)
+#   puts "Home Snnipet criado com sucesso"
+# else
+#   home_snippets = "Arquivo HTML não encontrado"
+#   puts "Home Snnipet não foi criado"
+# end
+
+# settings.merge!(html_content_pt: home_snippets)
+
+# html_content_block.(settings)
+# html_content_block.save!
+
+# Adicionando processos
+
