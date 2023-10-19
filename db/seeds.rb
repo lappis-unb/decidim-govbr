700d20b406a1e80e15fc4a7cbd93d4eeee4431e8
viniciusroriz <fowardshift@gmail.com>
2023-10-19 08:39:10 -0300

    pequenas alterações

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,24 @@
+# Cria o administrador do sistema
+Decidim::System::Admin.new(email: 'brasil@participativo.com', password: 'decidim123456789', password_confirmation: 'decidim123456789').save!(validate: false)
+
 # Define cores padrões do Brasil Participativo
 colors = {
-    primary: "#1351b4",
-    secondary: "#2378c3",
-    success: "#168821",
-    warning: "#2670e8",
-    alert: "#e5220a",
-    highlight: "#be6400",
-    theme: "#1351b4"
+    primary: '#1351b4',
+    secondary: '#2378c3',
+    success: '#168821',
+    warning: '#2670e8',
+    alert: '#e5220a',
+    highlight: '#be6400',
+    theme: '#1351b4'
 }
 
 # Snippets do Cabeçalho
-
 arquivo = Rails.root.join(__dir__, 'seeds', 'snippet.html')
 header_snippets = File.read(arquivo)
-puts "Header Snnipet criado com sucesso"
+puts 'Header Snippet criado com sucesso.'
 
 # Cria a organização Brasil Participativo
 organization = Decidim::Organization.find_by(name: 'Brasil Participativo')
-
 if !organization
     organization = Decidim::Organization.create!(
         name: 'Brasil Participativo',
@@ -32,25 +33,17 @@ if !organization
         user_groups_enabled: true,
         send_welcome_notification: true,
         file_upload_settings: Decidim::OrganizationSettings.default(:upload),
-        external_domain_whitelist: ["decidim.org", "github.com"],
+        external_domain_whitelist: ['decidim.org', 'github.com'],
         colors: colors,
         header_snippets: header_snippets,
     )
-    brasil_participativo.save!
-    puts "Organização 'Brasil Participativo' criada."
-# else
-#     organization.update!(
-#         {
-#             colors: colors,
-#             default_locale: 'pt-BR',
-#             available_locales: ['en', 'pt-BR']
-#         }
-#     )
+    organization.save!
+    puts 'Organização "Brasil Participativo" criada.'
 end
 
 # Cria um admin e relaciona com a organização 
 admin = Decidim::User.find_or_initialize_by(email: 'brasil@participativo.com')
-puts "Usuário com o e-mail brasil@participativo criado ou encontrado."
+puts 'Usuário com o e-mail brasil@participativo criado ou encontrado.'
 
 admin_hash = {
     name: 'Admin Brasil Participativo',
@@ -64,28 +57,24 @@ admin_hash = {
     accepted_tos_version: organization.tos_version + 1.hour,
     newsletter_notifications_at: Time.current,
     password_updated_at: Time.current,
-    admin_terms_accepted_at: Time.current
+    admin_terms_accepted_at: Time.current,
 }
 
-admin_hash.merge!(password: "decidim123456789") if admin.encrypted_password.blank?
+admin_hash.merge!(password: 'decidim123456789') if admin.encrypted_password.blank?
 admin.update!(admin_hash)
-puts "Processo de administrador do sistema concluído com sucesso"
+puts 'Processo de administrador do sistema concluído com sucesso.'
 
-# Criando Blocos de Conteúdo ativo da organização
-
-# Decidim::System::CreateDefaultContentBlocks.call(organization)
+# Cria Blocos de Conteúdo ativo da organização
 home_arquivo = Rails.root.join(__dir__, 'seeds', 'home-page.html') 
 home_snippets = File.read(home_arquivo)
-puts "Home Snnipet criado com sucesso"
+puts 'Home Snippet criado com sucesso.'
 
-# Cria a organização Brasil Participativo
-content_block = Decidim::ContentBlock.find_by(manifest_name: "html")
-
+content_block = Decidim::ContentBlock.find_by(manifest_name: 'html')
 content_block = Decidim::ContentBlock.create!(
     id: 1, 
     decidim_organization_id: 1,
-    manifest_name: "html",
-    scope_name: "homepage",
+    manifest_name: 'html',
+    scope_name: 'homepage',
     settings: { 
         html_content_pt: home_snippets,
     },
@@ -96,13 +85,11 @@ content_block = Decidim::ContentBlock.create!(
     created_at: Time.current,
     updated_at: Time.current   
 )
-    content_block.save!
-    puts "Bloco 'Bloco HTML' ativo"
-
-html_content_block = Decidim::ContentBlock.find_by(organization: organization, manifest_name: :html, scope_name: :homepage)
+content_block.save!
+puts 'Bloco "Bloco HTML" ativo.'
 
 # Snippets da home page
-
+html_content_block = Decidim::ContentBlock.find_by(organization: organization, manifest_name: :html, scope_name: :homepage)
 html_content_block.save!
 
 # Adicionando processos
