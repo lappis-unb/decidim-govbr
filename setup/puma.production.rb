101296e393f4d3087e86ec700e6f1e841ecdd0d2
David Carlos <ddavidcarlos1392@gmail.com>
2023-11-30 16:41:20 -0300

    Updates puma.production.rb

--- setup/puma.production.rb
+++ setup/puma.production.rb
@@ -1,26 +1,26 @@
 # Change to match your CPU core count
-workers 16
+workers 3
 
 # Min and Max threads per worker
-threads 10, 20
+threads 5, 10
 
 # Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
+rails_env = ENV['RAILS_ENV'] || 'production'
 environment rails_env
 
 # Set up socket location
-bind "unix:///tmp/sockets/puma.sock"
+# bind "unix:///tmp/sockets/puma.sock"
+bind 'tcp://0.0.0.0:9292'
 
 # Logging
-stdout_redirect "/var/log/puma.stdout.log", "/var/log/puma.stderr.log", true
+stdout_redirect '/var/log/puma.stdout.log', '/var/log/puma.stderr.log', true
 
 # Set master PID and state locations
-pidfile "/tmp/pids/puma.pid"
-state_path "/tmp/pids/puma.state"
+pidfile '/tmp/pids/puma.pid'
+state_path '/tmp/pids/puma.state'
 activate_control_app
 
 on_worker_boot do
-  require "active_record"
+  require 'active_record'
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  #ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
