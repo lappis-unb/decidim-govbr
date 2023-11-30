42f540191488d7340b7ff55b85e74ec5ee594027
David Carlos <ddavidcarlos1392@gmail.com>
2023-11-30 14:05:52 -0300

    Adds Dockerfile.prod

--- setup/puma.production.rb
+++ setup/puma.production.rb
@@ -4,21 +4,19 @@ workers 16
 # Min and Max threads per worker
 threads 10, 20
 
-app_dir = "/srv/decide"
-
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+bind "unix:///tmp/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+stdout_redirect "/var/log/puma.stdout.log", "/var/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
+pidfile "/tmp/pids/puma.pid"
+state_path "/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
