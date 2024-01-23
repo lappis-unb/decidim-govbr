1631fe9ba9d27b77dfc372e1e3e1ba4d1b8eb6b6
MaiconMares <maiconmaresunb@gmail.com>
2024-01-23 16:26:48 -0300

    Adding pluralized_name field in decidim_components
    Co-authored-by: Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_28_211231) do
+ActiveRecord::Schema.define(version: 2024_01_23_143845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -451,6 +451,7 @@ ActiveRecord::Schema.define(version: 2023_11_28_211231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "participatory_space_type", null: false
+    t.jsonb "pluralized_name"
     t.index ["participatory_space_id", "participatory_space_type"], name: "index_decidim_components_on_decidim_participatory_space"
   end
 
