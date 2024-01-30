9a4306e8863f9398576e32c7745d3f1d8f1c6a77
MaiconMares <maiconmaresunb@gmail.com>
2024-01-30 16:54:34 -0300

    Adding is_interactive field to DecidimProposalsProposal
    Co-authored-by: Leonardo Lago <leleolagomoreno@gmail.com>

---
+++ db/migrate/20240130165801_add_is_interactive_to_decidim_proposals_proposals.rb
@@ -0,0 +1,5 @@
+class AddIsInteractiveToDecidimProposalsProposals < ActiveRecord::Migration[6.1]
+  def change
+    add_column :decidim_proposals_proposals, :is_interactive, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_28_211231) do
+ActiveRecord::Schema.define(version: 2024_01_30_165801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -451,6 +451,7 @@ ActiveRecord::Schema.define(version: 2023_11_28_211231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "participatory_space_type", null: false
+    t.jsonb "singular_name"
     t.index ["participatory_space_id", "participatory_space_type"], name: "index_decidim_components_on_decidim_participatory_space"
   end
 
@@ -1621,6 +1622,7 @@ ActiveRecord::Schema.define(version: 2023_11_28_211231) do
     t.jsonb "body"
     t.integer "comments_count", default: 0, null: false
     t.integer "follows_count", default: 0, null: false
+    t.boolean "is_interactive"
     t.index "md5((body)::text)", name: "decidim_proposals_proposal_body_search"
     t.index "md5((title)::text)", name: "decidim_proposals_proposal_title_search"
     t.index ["created_at"], name: "index_decidim_proposals_proposals_on_created_at"
