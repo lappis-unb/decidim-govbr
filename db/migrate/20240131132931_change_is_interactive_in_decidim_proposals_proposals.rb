ada52118c41ca6ea37d5fe2f50c55c32ce49eede
MaiconMares <maiconmaresunb@gmail.com>
2024-02-07 10:26:41 -0300

    feat: unindo migraçoes em apenas uma
    Co-authored-by: Leonardo Lago <leleolagomoreno@gmail.com>
    Co-authored-by: Guilherme Monteiro <222006777@aluno.unb.br>

--- db/migrate/20240131132931_change_is_interactive_in_decidim_proposals_proposals.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIsInteractiveInDecidimProposalsProposals < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :decidim_proposals_proposals, :is_interactive, true
-  end
-end
