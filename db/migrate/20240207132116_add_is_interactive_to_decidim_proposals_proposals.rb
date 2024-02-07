944496f73b7d67439bf1b0dc3bdb37bae32c0219
MaiconMares <maiconmaresunb@gmail.com>
2024-02-07 12:01:21 -0300

    feat: removendo opcoes inutilizadas na migracao.

--- db/migrate/20240207132116_add_is_interactive_to_decidim_proposals_proposals.rb
+++ db/migrate/20240207132116_add_is_interactive_to_decidim_proposals_proposals.rb
@@ -1,5 +1,5 @@
 class AddIsInteractiveToDecidimProposalsProposals < ActiveRecord::Migration[6.1]
   def change
-    add_column :decidim_proposals_proposals, :is_interactive, :boolean, default: true, from: nil, to: true
+    add_column :decidim_proposals_proposals, :is_interactive, :boolean, default: true
   end
 end
