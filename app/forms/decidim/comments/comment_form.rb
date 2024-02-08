71a4826f043a0b2bba4d15fe46a59264f25ea710
Leonardo Lago Moreno <leleolagomoreno@gmail.com>
2024-02-08 13:11:51 -0300

    feat: utilizando form para atualizar status do comentário
    Co-authored-by: Maicon Mares <maiconmaresunb@gmail.com>
    Co-authored-by: Guilherme Monteiro <222006777@aluno.unb.br>

--- app/forms/decidim/comments/comment_form.rb
+++ app/forms/decidim/comments/comment_form.rb
@@ -10,7 +10,7 @@ module Decidim
       attribute :user_group_id, Integer
       attribute :commentable
       attribute :commentable_gid
-      attribute :status, Integer
+      attribute :status, String
 
       mimic :comment
 
