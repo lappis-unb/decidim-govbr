581ae8d8d4ea5dd4b2f3598758858d329a789808
VictorJorgeFGA <victor.eng.unb@gmail.com>
2024-02-10 13:21:06 -0300

    test: add tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,3 +7,18 @@ require "decidim/proposals/test/factories"
 require "decidim/comments/test/factories"
 require "decidim/accountability/test/factories"
 require "decidim/meetings/test/factories"
+
+FactoryBot.define do
+  factory :partner, class: "Decidim::Govbr::Partner" do
+    name { "partner" }
+    weight { 1 }
+
+    trait :as_organizer do
+      partner_type { "organizer" }
+    end
+
+    trait :as_supporter do
+      partner_type { "supporter" }
+    end
+  end
+end
