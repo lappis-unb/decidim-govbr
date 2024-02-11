31fa2b054b22f2221a1aeaa83988466d1a85d1c6
VictorJorgeFGA <victor.eng.unb@gmail.com>
2024-02-11 18:12:00 -0300

    feat: add media link form and create action

---
+++ spec/presenters/decidim/govbr/admin_log/media_link_presenter_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Decidim::Govbr::AdminLog::MediaLinkPresenter, type: :helper do
+  context "when participatory space is participatory process" do
+    let(:participatory_space) { create(:participatory_process, organization: organization) }
+    let(:admin_log_resource) { create(:govbr_media_link, participatory_space: participatory_space) }
+    let(:action) { "delete" }
+
+    include_examples "present admin log entry"
+  end
+
+  context "when participatory space is assembly" do
+    let(:participatory_space) { create(:assembly, organization: organization) }
+    let(:admin_log_resource) { create(:govbr_media_link, participatory_space: participatory_space) }
+    let(:action) { "delete" }
+
+    include_examples "present admin log entry"
+  end
+end
