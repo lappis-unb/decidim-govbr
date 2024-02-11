4a8c0a2777c9b95119ad0fc615602af4033198ce
VictorJorgeFGA <victor.eng.unb@gmail.com>
2024-02-11 18:17:15 -0300

    feat: add destroy media link command

--- spec/presenters/decidim/govbr/admin_log/media_link_presenter_spec.rb
+++ spec/presenters/decidim/govbr/admin_log/media_link_presenter_spec.rb
@@ -2,20 +2,26 @@
 
 require "rails_helper"
 
-describe Decidim::Govbr::AdminLog::MediaLinkPresenter, type: :helper do
-  context "when participatory space is participatory process" do
-    let(:participatory_space) { create(:participatory_process, organization: organization) }
-    let(:admin_log_resource) { create(:govbr_media_link, participatory_space: participatory_space) }
-    let(:action) { "delete" }
+module Decidim
+  module Govbr
+    module AdminLog
+      describe MediaLinkPresenter, type: :helper do
+        context "when participatory space is participatory process" do
+          let(:participatory_space) { create(:participatory_process, organization: organization) }
+          let(:admin_log_resource) { create(:govbr_media_link, participatory_space: participatory_space) }
+          let(:action) { "delete" }
 
-    include_examples "present admin log entry"
-  end
+          include_examples "present admin log entry"
+        end
 
-  context "when participatory space is assembly" do
-    let(:participatory_space) { create(:assembly, organization: organization) }
-    let(:admin_log_resource) { create(:govbr_media_link, participatory_space: participatory_space) }
-    let(:action) { "delete" }
+        context "when participatory space is assembly" do
+          let(:participatory_space) { create(:assembly, organization: organization) }
+          let(:admin_log_resource) { create(:govbr_media_link, participatory_space: participatory_space) }
+          let(:action) { "delete" }
 
-    include_examples "present admin log entry"
+          include_examples "present admin log entry"
+        end
+      end
+    end
   end
 end
