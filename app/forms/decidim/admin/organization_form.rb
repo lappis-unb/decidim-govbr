bc5f389b1218496a97fd6873361107bd037ab54b
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2024-02-20 14:32:47 -0300

    feat: começando o botão de autofill n oadmin

--- app/forms/decidim/admin/organization_form.rb
+++ app/forms/decidim/admin/organization_form.rb
@@ -77,6 +77,30 @@ module Decidim
         end
       end
 
+      def self.get_components_links(spaces, space_label)
+        spaces_links = spaces.map do |space|
+          links = space.components.map do |component|
+            label = component.respond_to?('name') ? 'name' : 'title'
+            label_pt_br = component.send(label)['pt-br'] || component.send(label)['pt'] || 'Componente'
+            url = "/#{space_label}/#{space.slug}/f/#{component.id}"
+            { 'id' => (label_pt_br + '_' + component.id.to_s), 'label' => label_pt_br, 'href' => url, "is_visible"=> true }
+          end
+          label_pt_br = space.send('title')['pt-br'] || space.send('title')['pt'] || 'Espaço'
+          { 'id' => (space_label + '_' + space.id.to_s), 'label' => label_pt_br, 'sub_items' => links, "is_visible"=> true }
+        end
+        spaces_links
+      end
+
+      def self.generate_menu_links
+        processes = get_components_links(Decidim::ParticipatoryProcess.all, 'processes')
+
+        { 'menu' => processes }.to_json.to_s.gsub(':', '=>')
+      end
+
+      def self.update_menu_links(updated_menu_links)
+        Decidim::Organization.find(1).update(menu_links: updated_menu_links)
+      end
+
       private
 
       def available_locales
