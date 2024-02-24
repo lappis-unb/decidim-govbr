f80161cfb60903ab7ea6c819030af5f5fc3e780f
GeovaneSFT <galefreitas@gmail.com>
2024-02-24 18:15:11 -0300

    Co-authored-by: Juan Pablo <Juan-Ricarte@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 require 'sidekiq/web'
 
-Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
-                                              ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
-                                                ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
-end if Rails.env.production?
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                  ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
+  end
+end
 
 Rails.application.routes.draw do
   mount Sidekiq::Web, at: '/sidekiq'
