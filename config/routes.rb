f6f839283247e6e113d439e224a9a24e6df5baf6
GeovaneSFT <galefreitas@gmail.com>
2024-02-24 18:15:11 -0300

    Co-authored-by: Juan Pablo <Juan-Ricarte@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 require 'sidekiq/web'
 
-Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
-                                              ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
-                                                ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
-end if Rails.env.production?
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                  ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
+  end
+end
 
 Rails.application.routes.draw do
   mount Sidekiq::Web, at: '/sidekiq'
@@ -14,11 +18,16 @@ Rails.application.routes.draw do
 
   mount Decidim::Core::Engine => '/'
 
-  get 'admin/user_proposal_statistic_report/:slug', to: 'decidim/govbr/user_proposals_statistic_settings#export_user_data', as: 'user_proposal_statistic_report'
+  get 'admin/user_proposal_statistic_report/:slug',
+      to: 'decidim/govbr/user_proposals_statistic_settings#export_user_data', as: 'user_proposal_statistic_report'
 
   # These two routes are not present anywhere in the product
   # Instead, it's meant to be used by an advanced admin directly on URL
   # This is going to be removed once admin front-end is finished
-  get 'admin/user_proposal_statistic_report_force_refresh/:slug', to: 'decidim/govbr/user_proposals_statistic_settings#force_refresh', as: 'user_proposal_statistic_report_force_refresh'
-  get 'admin/user_proposal_statistic_report_create/:slug', to: 'decidim/govbr/user_proposals_statistic_settings#create', as: 'user_proposal_statistic_report_create'
+  get 'admin/user_proposal_statistic_report_force_refresh/:slug',
+      to: 'decidim/govbr/user_proposals_statistic_settings#force_refresh', as: 'user_proposal_statistic_report_force_refresh'
+  get 'admin/user_proposal_statistic_report_create/:slug',
+      to: 'decidim/govbr/user_proposals_statistic_settings#create', as: 'user_proposal_statistic_report_create'
+  post 'admin/organization/homepage/edit', to: 'decidim/admin/organization_homepage#add_html_block',
+                                           as: 'add_html_block'
 end
