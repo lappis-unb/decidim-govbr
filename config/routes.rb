f9f2c9769bdf9cf0a215cbac1d38669132b6070f
GeovaneSFT <galefreitas@gmail.com>
2024-02-26 14:23:08 -0300

    style: lint

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,6 @@ Rails.application.routes.draw do
     end
   end
 
-
-
   get 'admin/user_proposal_statistic_report/:slug', to: 'decidim/govbr/user_proposals_statistic_settings#export_user_data', as: 'user_proposal_statistic_report'
 
   # These two routes are not present anywhere in the product
