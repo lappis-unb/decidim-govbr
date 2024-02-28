04054e9168dbf823518f0aaaafcb474f4196bfae
MaiconMares <maiconmaresunb@gmail.com>
2024-02-28 17:06:57 -0300

    feat: implementando caso de teste para verificar quando usuario comum incorpora/rejeita comentario.

--- spec/commands/decidim/comments/update_comment_status_spec.rb
+++ spec/commands/decidim/comments/update_comment_status_spec.rb
@@ -8,7 +8,8 @@ module Decidim
       let(:organization) { create(:organization) }
       let(:participatory_process) { create(:participatory_process, organization: organization) }
       let(:component) { create(:component, participatory_space: participatory_process) }
-      let(:author) { create(:user, organization: organization) }
+      let(:author) { create(:user, organization: organization, admin: true) }
+      let(:common_user) { create(:user, organization: organization, admin: false)}
       let(:dummy_resource) { create :dummy_resource, component: component }
       let(:commentable) { dummy_resource }
       let(:comment) { create :comment, author: author, commentable: commentable }
@@ -25,7 +26,7 @@ module Decidim
       let(:command) { described_class.new(comment, current_user, params) }
 
       describe "call" do
-        describe "when the form is valid" do
+        describe "when user is admin" do
           it "broadcasts ok" do
             expect { command.call }.to broadcast(:ok)
           end
@@ -34,6 +35,14 @@ module Decidim
             command.call
             comment.reload
             expect(comment.status).to eq("accepted")
+          end
+        end
+
+        describe "when user is not an admin" do
+          let(:invalid_command) { described_class.new(comment, common_user, params) }
+
+          it "broadcasts invalid" do
+            expect { invalid_command.call }.to broadcast(:invalid)
           end
         end
       end
