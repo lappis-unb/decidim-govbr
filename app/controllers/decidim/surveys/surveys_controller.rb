d3678bdb051fafa23dc9d05dedf306b7d30d65de
MaiconMares <maiconmaresunb@gmail.com>
2024-03-07 16:25:30 -0300

    feat: adicionando verificacao se usuario ja preencheu enquete de Perfil de Usuario ao preencher enquetes.

--- app/controllers/decidim/surveys/surveys_controller.rb
+++ app/controllers/decidim/surveys/surveys_controller.rb
@@ -12,7 +12,8 @@ module Decidim
 
       before_action :check_permissions
 
-      before_action :update_user_poll_answered, only: [:answer], if: -> { should_have_user_full_profile }
+      before_action :update_user_poll_answered, only: [:answer], if: -> { should_update_user_poll_answered}
+      before_action :should_have_user_full_profile, only: [:show]
 
       def check_permissions
         render :no_permission unless action_authorized_to(:answer, resource: survey).ok?
@@ -46,11 +47,21 @@ module Decidim
         current_user.update!(user_profile_poll_answered: true)
       end
 
-      def should_have_user_full_profile
+      def should_update_user_poll_answered
         expected_poll_link = "/processes/" + params[:participatory_process_slug] + "/f/" + params[:component_id]
 
         (current_organization.user_profile_poll_link.include? expected_poll_link) && 
         current_participatory_space.should_have_user_full_profile
+      end
+
+      def should_have_user_full_profile
+        expected_poll_link = "/processes/" + params[:participatory_process_slug] + "/f/" + params[:component_id]
+
+        if ((!current_organization.user_profile_poll_link.include? expected_poll_link) && 
+        current_participatory_space.should_have_user_full_profile && !current_user.user_profile_poll_answered)
+          flash[:alert] = I18n.t("decidim.components.proposals.actions.action_not_allowed")
+          flash[:poll_link] = current_organization.user_profile_poll_link.to_s
+        end
       end
     end
   end
