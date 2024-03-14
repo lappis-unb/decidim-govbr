f3ef8ecea3495600feceea45d0638bb3e9d4948f
MaiconMares <maiconmaresunb@gmail.com>
2024-03-14 12:10:11 -0300

    fix: refatorando nome de metodo helper para melhor entendimento do usuario.

--- app/controllers/decidim/surveys/surveys_controller.rb
+++ app/controllers/decidim/surveys/surveys_controller.rb
@@ -56,11 +56,11 @@ module Decidim
       end
 
       def should_have_user_full_profile
-        return unless current_user.present?
+        return unless current_participatory_space.is_a? Decidim::ParticipatoryProcess
 
         if (current_organization.user_profile_survey_id != current_component.id) && 
             current_participatory_space.should_have_user_full_profile && 
-            !current_user.user_profile_poll_answered
+            current_user.present? && !current_user.user_profile_poll_answered
           survey_component_id = current_organization.user_profile_survey_id
           
           flash[:alert] = I18n.t("decidim.components.proposals.actions.action_not_allowed")
--- app/helpers/decidim/govbr/participatory_processes_helper.rb
+++ app/helpers/decidim/govbr/participatory_processes_helper.rb
@@ -5,7 +5,7 @@ module Decidim
         current_user.present? && current_user.user_profile_poll_answered
       end
 
-      def user_has_full_profile?
+      def user_full_profile_required?
         current_user.present? && current_participatory_space.should_have_user_full_profile
       end
     end
