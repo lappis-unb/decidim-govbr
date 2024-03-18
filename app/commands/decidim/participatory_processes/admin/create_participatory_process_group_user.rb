1d29ad91a12c285cded598d8697a9afb99e3da36
VictorJorgeFGA <victor.eng.unb@gmail.com>
2024-03-18 00:52:37 -0300

    feat: create controller and commands for process group users handling

---
+++ app/commands/decidim/participatory_processes/admin/create_participatory_process_group_user.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    module Admin
+      # This command links user to participatory process group by
+      # creating roles
+      class CreateParticipatoryProcessGroupUser < Decidim::Command
+        # Public: Initializes the command
+        #
+        # participatory_process_group - the participatory process group to link
+        # current_user - the current user that is perfoming the action
+        # form - A form object containing user information and relationship, i.e.: user role
+        def initialize(participatory_process_group, current_user, form)
+          @participatory_process_group = participatory_process_group
+          @current_user = current_user
+          @form = form
+        end
+
+        attr_reader :participatory_process_group, :current_user, :form
+
+        # Executes the command. Broadcasts these events:
+        #
+        # - :ok when everything is valid.
+        # - :invalid if form has invalid data, or participatory process group is blank, or user already belong to a group
+        # - :taken when user already belongs to a process group
+        #
+        # Returns nothing
+        def call
+          return broadcast(:invalid) if form.invalid? || participatory_process_group.blank?
+          return broadcast(:taken, user_current_process_group.title[current_locale]) if user_current_process_group.present?
+
+          transaction do
+            update_user_roles!
+
+            user.participatory_process_group = participatory_process_group
+            user.decidim_participatory_process_group_role = form.role
+            user.save!
+          end
+
+          broadcast(:ok)
+        end
+
+        def user_current_process_group
+          user.participatory_process_group
+        end
+
+        def user
+          @user ||= Decidim::User.find_by(email: form.email)
+        end
+
+        def scoped_user_roles
+          @scoped_user_roles ||= ParticipatoryProcessUserRole.where(user: user, participatory_processes: participatory_processes)
+        end
+
+        def participatory_processes
+          @participatory_processes ||= participatory_process_group.participatory_processes
+        end
+
+        def update_user_roles!
+          destroy_current_user_roles!
+          create_new_user_roles!
+        end
+
+        def destroy_current_user_roles!
+          scoped_user_roles.each do |user_role|
+            Decidim.traceability.perform_action!(
+              "delete",
+              user_role,
+              current_user,
+              resource: {
+                title: user_role.user.name
+              }
+            ) do
+              user_role.destroy!
+              user_role
+            end
+          end
+        end
+
+        def create_new_user_roles!
+          participatory_processes.each do |participatory_process|
+            role_params = {
+              role: form.role.to_sym,
+              user: user,
+              participatory_process: participatory_process
+            }
+
+            Decidim.traceability.create!(
+              Decidim::ParticipatoryProcessUserRole,
+              current_user,
+              role_params,
+              resource: {
+                title: user.name
+              }
+            )
+          end
+        end
+      end
+    end
+  end
+end
---
+++ app/controllers/decidim/participatory_processes/admin/participatory_process_group_users_controller.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    module Admin
+      # Controller to handle participatory process group users creation and destruction
+      #
+      class ParticipatoryProcessGroupUsersController < Decidim::ParticipatoryProcesses::Admin::ApplicationController
+        layout "decidim/admin/participatory_process_group"
+
+        helper_method :participatory_process_group
+
+        def index
+          enforce_permission_to :read, :process_user_role
+
+          @users = collection
+        end
+
+        def new
+          enforce_permission_to :create, :process_user_role
+
+          @form = form(ParticipatoryProcessGroupUserForm).instance
+        end
+
+        def create
+          enforce_permission_to :create, :process_user_role
+
+          @form = form(ParticipatoryProcessGroupUserForm).from_params(params)
+          CreateParticipatoryProcessGroupUser.call(participatory_process_group, current_user, @form) do
+            on(:ok) do
+              flash[:notice] = I18n.t("participatory_process_group_users.create.success", scope: "decidim.admin")
+              redirect_to main_app.participatory_process_group_users_path(participatory_process_group)
+            end
+
+            on(:invalid) do
+              flash[:alert] = I18n.t("participatory_process_group_users.create.error", scope: "decidim.admin")
+              render :new
+            end
+
+            on(:taken) do |group_name|
+              flash[:alert] = I18n.t("participatory_process_group_users.create.taken", scope: "decidim.admin", group_name: group_name)
+              render :new
+            end
+          end
+        end
+
+        def destroy
+          enforce_permission_to :create, :process_user_role
+
+          @user = collection.find(params[:id])
+          DestroyParticipatoryProcessGroupUser.call(participatory_process_group, current_user, @user) do
+            on(:ok) do
+              flash[:notice] = I18n.t("participatory_process_group_users.destroy.success", scope: "decidim.admin")
+              redirect_to main_app.participatory_process_group_users_path(participatory_process_group)
+            end
+
+            on(:invalid) do
+              flash[:alert] = I18n.t("participatory_process_group_users.destroy.error", scope: "decidim.admin")
+              redirect_to main_app.participatory_process_group_users_path(participatory_process_group)
+            end
+          end
+        end
+
+        private
+
+        def participatory_process_group
+          @participatory_process_group ||= OrganizationParticipatoryProcessGroups.new(current_user.organization).query.find(params[:participatory_process_group_id])
+        end
+
+        def collection
+          @collection ||= Decidim::User.where(participatory_process_group: participatory_process_group)
+        end
+      end
+    end
+  end
+end
