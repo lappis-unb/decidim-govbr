858687306f6d28f825aa4ed68343a40eb08c55b4
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2024-03-27 23:46:50 -0300

    feat: agora o formulario de criação tem tema e anexos

--- app/forms/decidim/proposals/proposal_wizard_create_step_form.rb
+++ app/forms/decidim/proposals/proposal_wizard_create_step_form.rb
@@ -4,18 +4,27 @@ module Decidim
   module Proposals
     # A form object to be used when public users want to create a proposal.
     class ProposalWizardCreateStepForm < Decidim::Form
+      include Decidim::AttachmentAttributes
+
       mimic :proposal
 
       attribute :title, String
       attribute :body, Decidim::Attributes::CleanString
       attribute :body_template, String
       attribute :user_group_id, Integer
+      attribute :category_id, Integer
+      attribute :attachment, AttachmentForm
+
+      attachments_attribute :photos
+      attachments_attribute :documents
 
       validates :title, :body, presence: true, etiquette: true
       validates :title, length: { in: 15..150 }
       validates :body, length: { in: 15..5000 }
 
       validate :body_is_not_bare_template
+
+      delegate :categories, to: :current_component
 
       alias component current_component
 
@@ -27,6 +36,12 @@ module Decidim
         return unless model.categorization
 
         self.category_id = model.categorization.decidim_category_id
+        self.photos = [model.photo].compact.select { |p| p.weight.zero? }
+        self.documents = model.attachments - photos
+      end
+
+      def category
+        @category ||= categories.find_by(id: category_id)
       end
 
       private
