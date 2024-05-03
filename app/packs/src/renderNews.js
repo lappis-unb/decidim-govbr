b7a4e91ed9557832828c63a43f1197775a5a1b64
Leonardo Lago Moreno <leleolagomoreno@gmail.com>
2024-05-03 11:48:16 -0300

    feat: adicionando funções de renderizar noticias em pages

---
+++ app/packs/src/renderNews.js
@@ -0,0 +1,133 @@
+function fetchGraphQLData(url, query) {
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  const body = JSON.stringify({ query });
+
+  return fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: body,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw new Error(`Error in GraphQL query: ${error.message}`);
+    });
+}
+
+function renderNews(apiData, href) {
+  const newsData = apiData.data.participatoryProcesses;
+  const resultHTMLArray = [];
+
+  newsData.forEach((process) => {
+    process.components.forEach((component) => {
+      if (component.posts) {
+        component.posts.nodes.forEach((post) => {
+          const imageUrl = post.attachments.length > 0 ? post.attachments[0].thumbnail : "";
+          const newHTML = `
+          <a href="${href}" class="news-card">
+            <img src="${imageUrl}" alt="" class="news-img"/>
+            <span class="news-title">${post.title.translation}</span>
+          </a>
+          `;
+
+          resultHTMLArray.push(newHTML);
+        });
+      }
+    });
+  });
+
+  if (resultHTMLArray.length === 0) {
+    return "error";
+  }
+
+  return resultHTMLArray.join("\n");
+}
+
+function manageNews(apiUrl, href, idComponent, idProcess, title) {
+  const confNoticias = document.querySelector("#confnoticias");
+  if (!confNoticias) {
+    console.error('Elemento com id "confnoticias" não encontrado.');
+    return;
+  }
+  
+  confNoticias.innerHTML = `<section id="news-home-section" class="home-sections-container">
+                                <h2>${title}</h2>
+                                <div class="home-sections-content br-container-lg">
+                                    <div class="news-container">
+                                        <span class="news-title">Carregando...</span>
+                                    </div>
+                                </div>
+                            </section>`; 
+
+  const query = `query {
+    participatoryProcesses(filter: { id: ${idProcess} }) {
+      components(filter: { id: ${idComponent} }) {
+        ...on Blogs {
+          posts(order: {createdAt: "DESC"}, first: 3) {
+            nodes {
+              title {
+                translation(locale: "pt-BR")
+              }
+              body {
+                translation(locale: "pt-BR")
+              }
+              attachments {
+                thumbnail
+              }
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+  fetchGraphQLData(apiUrl, query)
+    .then((apiData) => renderNews(apiData, href))
+    .then((renderedNewsHTML) => {
+      if (renderedNewsHTML === "error") {
+        confNoticias.innerHTML = `<section id="news-home-section" class="home-sections-container">
+                                      <h2>${title}</h2>
+                                      <div class="home-sections-content br-container-lg">
+                                          <div class="news-container">
+                                              <span class="news-title">Nenhuma notícia encontrada</span>
+                                          </div>
+                                      </div>
+                                  </section>`; 
+      } else {
+        let contentNoticias = `<section id="news-home-section" class="home-sections-container">
+                                  <h2>${title}</h2>
+                                  <div class="home-sections-content br-container-lg">
+                                    <div class="news-container">
+                                      ${renderedNewsHTML}
+                                    </div>
+                                  </div>
+                               </section>`;
+        confNoticias.innerHTML = contentNoticias;
+      }
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+      confNoticias.innerHTML = `<section id="news-home-section" class="home-sections-container">
+                                    <h2>${title}</h2>
+                                    <div class="home-sections-content br-container-lg">
+                                        <div class="news-container">
+                                            <span>Erro ao buscar notícias.</span>
+                                        </div>
+                                    </div>
+                                </section>`; 
+    });
+}
+
+window.manageNews = {
+    manageNews: manageNews,
+};
+
+export { manageNews };
