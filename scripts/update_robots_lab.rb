7052f5412fa7a25b6037f35d50a91f38c7ac8758
Giovanni Alvissus <giovanni.acg@gmail.com>
2024-05-06 20:03:10 +0000

    feat: script to update robots

---
+++ scripts/update_robots_lab.rb
@@ -0,0 +1,19 @@
+# File: update_robots.rb
+require 'fileutils'
+
+# Path to the public directory where robots.txt is located
+public_dir = File.join(Rails.root, 'public')
+robots_file_path = File.join(public_dir, 'robots.txt')
+
+# New content for the robots.txt - more information in http://www.robotstxt.org/robotstxt.html
+new_content = <<-ROBOTS
+User-agent: *
+Disallow: /
+ROBOTS
+
+# Write the new content to robots.txt
+File.open(robots_file_path, 'w') do |file|
+  file.write(new_content)
+end
+
+puts "robots.txt has been updated."
