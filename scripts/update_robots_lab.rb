729b494fae8fdadc423b24772e0bd65b5b854595
Gustavo <gustavohenriqueprivado@gmail.com>
2024-05-06 21:11:16 -0300

    fix: lint

--- scripts/update_robots_lab.rb
+++ scripts/update_robots_lab.rb
@@ -6,14 +6,12 @@ public_dir = File.join(Rails.root, 'public')
 robots_file_path = File.join(public_dir, 'robots.txt')
 
 # New content for the robots.txt - more information in http://www.robotstxt.org/robotstxt.html
-new_content = <<-ROBOTS
-User-agent: *
-Disallow: /
+new_content = <<~ROBOTS
+  User-agent: *
+  Disallow: /
 ROBOTS
 
 # Write the new content to robots.txt
-File.open(robots_file_path, 'w') do |file|
-  file.write(new_content)
-end
+File.write(robots_file_path, new_content)
 
 puts "robots.txt has been updated."
