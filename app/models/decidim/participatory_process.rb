8416112299ee37cb257c003fdc3306ad700f5a84
GeovaneSFT <galefreitas@gmail.com>
2024-05-13 11:18:11 -0300

    feat: altera local do state process

--- app/models/decidim/participatory_process.rb
+++ app/models/decidim/participatory_process.rb
@@ -159,18 +159,6 @@ module Decidim
       start_date <= Date.current && (end_date.blank? || end_date >= Date.current)
     end
 
-    def state
-      if past_result?
-        "Encerrada - Resultado"
-      elsif past?
-        "Encerrada"
-      elsif upcoming?
-        "Não iniciada"
-      else
-        "Ativa"
-      end
-    end
-
     def past_result?
       return false if end_date.blank?
 
