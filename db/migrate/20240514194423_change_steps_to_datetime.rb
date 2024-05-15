9a4732ff7f7be8e2f3ecf10ecf443b8beb65534b
GeovaneSFT <galefreitas@gmail.com>
2024-05-15 15:05:03 -0300

    feat: migration date pra datetime

---
+++ db/migrate/20240514194423_change_steps_to_datetime.rb
@@ -0,0 +1,6 @@
+class ChangeStepsToDatetime < ActiveRecord::Migration[6.0]
+  def change
+    change_column :decidim_participatory_process_steps, :start_date, :datetime
+    change_column :decidim_participatory_process_steps, :end_date, :datetime
+  end
+end
