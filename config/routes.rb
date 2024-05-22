6ce6915648b41e32e9df9ee35f72ac0485323243
GeovaneSFT <galefreitas@gmail.com>
2024-05-22 16:41:44 -0300

    fix routes 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@
 
 require 'sidekiq/web'
 
-if Rails.env.production?
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
-                                                ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
-                                                  ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
-  end
-end
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                              ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
+end if Rails.env.production?
 
 Rails.application.routes.draw do
   mount Sidekiq::Web, at: '/sidekiq'
