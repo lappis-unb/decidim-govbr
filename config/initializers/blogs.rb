3d3d08faf44aa986de9dbcddcd4de294bf792aab
MaiconMares <maiconmaresunb@gmail.com>
2024-06-05 13:50:28 -0300

    fix: removendo campo inutilizado em blogs, meetings e proposals.

--- config/initializers/blogs.rb
+++
@@ -1,3 +0,0 @@
-Decidim.component_registry.find(:blogs).settings(:global) do |settings|
-  settings.attribute :enable_comments_attachment, type: :boolean, default: false
-end
--- config/initializers/meetings.rb
+++ config/initializers/meetings.rb
@@ -1,4 +1,3 @@
 Decidim.component_registry.find(:meetings).settings do |settings|
   settings.attribute :attachments_allowed, type: :boolean, default: false
-  settings.attribute :enable_comments_attachment, type: :boolean, default: false
 end
