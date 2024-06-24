8ba76460363eda0620af3fd810ee2c097866b6c4
MaiconMares <maiconmaresunb@gmail.com>
2024-06-24 09:51:40 -0300

    fix: adicionando anexos habilitados por padrao.

--- config/initializers/proposals.rb
+++ config/initializers/proposals.rb
@@ -1,6 +1,6 @@
 Decidim.component_registry.find(:proposals).settings(:global) do |settings|
   settings.attribute :most_voted_rule, type: :integer, default: 0
-  settings.attribute :enable_comments_attachment, type: :boolean, default: false
+  settings.attribute :enable_comments_attachment, type: :boolean, default: true
   settings.attribute :requires_previous_moderation, type: :boolean, default: false
 end
 
