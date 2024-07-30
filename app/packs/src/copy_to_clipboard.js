368683ca0e2e409aba97f5520d46db87c7c4dd92
raqueleucaria <raqueleucaria@gmail.com>
2024-07-30 10:39:24 -0300

    fix: apaga os espaços apor ";"

--- app/packs/src/copy_to_clipboard.js
+++ app/packs/src/copy_to_clipboard.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var clipboardButtons = document.getElementsByClassName("button copy-link-button"); 
-  for (var i = 0; i < clipboardButtons.length; i++) {
+  var clipboardButtons = document.getElementsByClassName("button copy-link-button");
+  for (var i = 0;i < clipboardButtons.length;i++) {
     clipboardButtons[i].addEventListener("click", function () {
-      var urlShareLink = document.getElementById("urlShareLink"); 
+      var urlShareLink = document.getElementById("urlShareLink");
       if (urlShareLink) {
         urlShareLink.select();
         navigator.clipboard.writeText(urlShareLink.value).then(function () {
           this.innerHTML = "Copiado! ✓";
-        }.bind(this)); 
+        }.bind(this));
       }
     });
   }
