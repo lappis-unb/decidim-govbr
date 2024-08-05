3ec6626b6ac2c42221bc95dc79067b5fb361316b
Eduardo Nunes <eduardonunes2525@gmail.com>
2024-08-05 17:31:44 -0300

    fix: Remove colunas que não existem em produção

--- db/migrate/20240801185543_removing_useless_fields_in_decidim_homes.rb
+++ db/migrate/20240801185543_removing_useless_fields_in_decidim_homes.rb
@@ -9,20 +9,5 @@ class RemovingUselessFieldsInDecidimHomes < ActiveRecord::Migration[6.1]
     remove_column :decidim_homes_homes, :statistics
     remove_column :decidim_homes_homes, :news
     remove_column :decidim_homes_homes, :news_id
-    remove_column :decidim_homes_homes, :header_title
-    remove_column :decidim_homes_homes, :header_subtitle
-    remove_column :decidim_homes_homes, :steps_title
-    remove_column :decidim_homes_homes, :steps_subtitle
-    remove_column :decidim_homes_homes, :emphasis_title
-    remove_column :decidim_homes_homes, :emphasis_subtitle
-    remove_column :decidim_homes_homes, :emphasis_button_text
-    remove_column :decidim_homes_homes, :participation_title
-    remove_column :decidim_homes_homes, :participation_subtitle
-    remove_column :decidim_homes_homes, :map_able
-    remove_column :decidim_homes_homes, :emphasis_link
-    remove_column :decidim_homes_homes, :meetings_map
-    remove_column :decidim_homes_homes, :meeting_id
-    remove_column :decidim_homes_homes, :reverse_call_to_action
-    rename_column :decidim_homes_homes, :field_orders, :element_orders
   end
 end
