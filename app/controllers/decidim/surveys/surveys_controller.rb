ee93f742145768ca30f6cc3f8ce1b2d4e78140ea
MaiconMares <maiconmaresunb@gmail.com>
2024-08-06 10:46:29 -0300

    feat: adicionando ao formulario de enquete campo hidden para armazenar id da reuniao de origem.

--- app/controllers/decidim/surveys/surveys_controller.rb
+++ app/controllers/decidim/surveys/surveys_controller.rb
@@ -15,6 +15,7 @@ module Decidim
 
       before_action :update_user_poll_answered, only: [:answer], if: -> { should_update_user_poll_answered }
       before_action :should_have_user_full_profile, only: [:show]
+      before_action :add_new_question_to_survey, only: [:show]
 
       def check_permissions
         render :no_permission unless action_authorized_to(:answer, resource: survey).ok?
@@ -65,6 +66,20 @@ module Decidim
 
           flash[:alert] = I18n.t("decidim.components.surveys.actions.action_not_allowed")
           flash[:poll_link] = mount_user_profile_survey_url(survey_id: survey_component_id)
+        end
+      end
+
+      def add_new_question_to_survey
+        record_what_happened_survey = current_participatory_space.record_what_happened_survey.presence.to_s.chomp("/")
+        if record_what_happened_survey.present? && request.original_url.include?(record_what_happened_survey)
+          questionnaire = survey.questionnaire
+
+          return false if questionnaire.blank?
+
+          Decidim::Forms::Questionnaire.add_new_question(
+            decidim_questionnaire_id: questionnaire.id, position: questionnaire.questions.count,
+            question_type: "short_answer", mandatory: false,title: "origin_meeting_id",
+          )
         end
       end
     end
