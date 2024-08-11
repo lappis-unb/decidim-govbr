edd2d858b317b72a9a8446422d8b096b621d7e42
GeovaneSFT <galefreitas@gmail.com>
2024-08-11 01:15:12 -0300

    fix: loading infinito

--- app/packs/src/decidim/participatory_processes/filters.js
+++ app/packs/src/decidim/participatory_processes/filters.js
@@ -1,39 +1,52 @@
 $(() => {
-    const $processesGrid = $("#processes-grid");
-    const $loading = $processesGrid.find(".loading");
-    const filterLinksSelector = ".order-by__tabs a";
-  
-    $loading.hide();
-  
-    $processesGrid.on("click", filterLinksSelector, (event) => {
-      const $processesGridCards = $processesGrid.find(".card-grid .column");
-      let $target = $(event.target);
-  
-      if (!$target.is("a")) {
-        $target = $target.parents("a");
-      }
-  
-      $(filterLinksSelector).removeClass("is-active");
-      $target.addClass("is-active");
-  
-      $processesGridCards.hide();
-      $loading.show();
-  
-      $.ajax({
-        type: "GET",
-        url: $target.attr("href"),
-        success: function(data) {
-          const $newCards = $(data).find(".card-grid .column");
-  
-          // Atualizar o conteúdo dos cards
-          $processesGridCards.html($newCards);
-          $processesGridCards.show();
-          $loading.hide();
-  
-          // Atualizar o texto do botão com o filtro atual
-          const currentFilterName = $target.text(); // Obter o texto do filtro selecionado
-          $("#button-text").text(currentFilterName); // Atualizar o botão
-        }
-      });
-    });
+  const $processesGrid = $("#processes-grid");
+  const $loading = $processesGrid.find(".loading");
+  const stateFilterLinksSelector = ".order-by__tabs a";
+  const typeFilterLinksSelector = "#process-type-filter a";
+  $loading.hide();
+
+  $processesGrid.on("click", "#inline-filter-sort a", (event) => {
+    handleFilterClick(event, stateFilterLinksSelector);
   });
+
+  $processesGrid.on("click", "#inline-state-filter-sort a", (event) => {
+    handleFilterClick(event, typeFilterLinksSelector);
+  });
+
+  function handleFilterClick(event, filterLinksSelector) {
+    const $processesGridCards = $processesGrid.find(".card-grid");
+    const $processesGridSecHeading = $processesGrid.find(".section-heading");
+    let $target = $(event.target);
+
+    if (!$target.is("a")) {
+      $target = $target.parents("a");
+    }
+
+    $(filterLinksSelector).removeClass("is-active");
+    $target.addClass("is-active");
+
+    $processesGridCards.hide();
+    $loading.show();
+
+    $.ajax({
+      type: "GET",
+      url: $target.attr("href"),
+      success: function (data) {
+        const $newCards = $(data).find(".card-grid");
+        const $newSecHeading = $(data).find(".section-heading");
+
+        // Update cards content
+        $processesGridSecHeading.html($newSecHeading);
+        $processesGridCards.html($newCards);
+        $processesGridCards.show();
+        $loading.hide();
+
+        const currentFilterName = $target.text();
+        $target
+          .parents(".inline-filters")
+          .find("#button-text")
+          .text(currentFilterName);
+      },
+    });
+  }
+});
