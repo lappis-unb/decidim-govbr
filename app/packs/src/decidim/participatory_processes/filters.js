486ee4c37f627dee466f0e2b944773d3ff885392
GeovaneSFT <galefreitas@gmail.com>
2024-08-11 02:00:34 -0300

    fix: dropdowns novos não abriam

--- app/packs/src/decidim/participatory_processes/filters.js
+++ app/packs/src/decidim/participatory_processes/filters.js
@@ -15,38 +15,42 @@ $(() => {
 
   function handleFilterClick(event, filterLinksSelector) {
     const $processesGridCards = $processesGrid.find(".card-grid");
-    const $processesGridSecHeading = $processesGrid.find(".section-heading");
+    const $processesOrderBy = $processesGrid.find(".processes-grid-order-by");
     let $target = $(event.target);
-
+  
     if (!$target.is("a")) {
       $target = $target.parents("a");
     }
-
+  
     $(filterLinksSelector).removeClass("is-active");
     $target.addClass("is-active");
-
+  
     $processesGridCards.hide();
     $loading.show();
-
+  
     $.ajax({
       type: "GET",
       url: $target.attr("href"),
       success: function (data) {
         const $newCards = $(data).find(".card-grid");
-        const $newSecHeading = $(data).find(".section-heading");
-
+        const $newOrderBy = $(data).find(".processes-grid-order-by");
+  
         // Update cards content
-        $processesGridSecHeading.html($newSecHeading);
+        $processesOrderBy.html($newOrderBy);
         $processesGridCards.html($newCards);
         $processesGridCards.show();
         $loading.hide();
-
+  
         const currentFilterName = $target.text();
         $target
           .parents(".inline-filters")
           .find("#button-text")
           .text(currentFilterName);
+  
+        // Reinitialize Foundation for newly loaded content
+        $(document).foundation();
       },
     });
   }
+  
 });
