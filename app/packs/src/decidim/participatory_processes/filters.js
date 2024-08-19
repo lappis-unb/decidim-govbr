ed868568ca1ca3acb277b1ce8f6a6873352db71a
GeovaneSFT <galefreitas@gmail.com>
2024-08-19 16:24:05 -0300

    tooltip e fix wrapper

--- app/packs/src/decidim/participatory_processes/filters.js
+++ app/packs/src/decidim/participatory_processes/filters.js
@@ -17,26 +17,26 @@ $(() => {
     const $processesGridCards = $processesGrid.find(".card-grid");
     const $processesOrderBy = $processesGrid.find(".processes-grid-order-by");
     let $target = $(event.target);
-  
+
     if (!$target.is("a")) {
       $target = $target.parents("a");
     }
-  
+
     $(filterLinksSelector).removeClass("is-active");
     $target.addClass("is-active");
-  
+
     $processesGridCards.hide();
     $loading.show();
-  
+
     $.ajax({
       type: "GET",
       url: $target.attr("href"),
       success: function (data) {
         const $newCards = $(data).find(".card-grid");
         const $newOrderBy = $(data).find(".processes-grid-order-by");
-  
+
         // Update cards content
-        $processesOrderBy.html($newOrderBy);
+        $processesOrderBy.replaceWith($newOrderBy);
         $processesGridCards.replaceWith($newCards);
         $processesGridCards.show();
         $loading.hide();
@@ -45,11 +45,58 @@ $(() => {
           .parents(".inline-filters")
           .find("#button-text")
           .text(currentFilterName);
-  
-        // Reinitialize Foundation(necessary for dropdowns to work again)
+
+        // Reinitialize Foundation (necessary for dropdowns to work again)
         $(document).foundation();
+
+        // Re-bind click event for the help text
+        bindHelpTextClick();
       },
     });
   }
-  
+
+  // Function to bind the click event to the help text
+  function bindHelpTextClick() {
+    const $helpText = $("#help-text");
+    const $announcementText = $("#announcement-text");
+
+    if ($helpText.length && $announcementText.length) {
+      $helpText.off("click").on("click", () => {
+        toggleHelpText();
+      });
+    }
+  }
+
+  function toggleHelpText() {
+    const $announcementText = $("#announcement-text");
+    const $helpText = $("#help-text");
+
+    const isHidden = $announcementText.hasClass("hidden");
+
+    if (isHidden) {
+      showHelpText();
+    } else {
+      hideHelpText();
+    }
+  }
+
+  function showHelpText() {
+    const $announcementText = $("#announcement-text");
+    const $helpText = $("#help-text");
+
+    $announcementText.removeClass("hidden").addClass("visible");
+    $helpText.html(`<i class="fa fa-info-circle size-icon-filter" aria-hidden="true"></i> ${$helpText.text().trim()} <i class="fa fa-chevron-up size-icon-filter"></i>`);
+    $(document).foundation();
+  }
+
+  function hideHelpText() {
+    const $announcementText = $("#announcement-text");
+    const $helpText = $("#help-text");
+
+    $announcementText.removeClass("visible").addClass("hidden");
+    $helpText.html(`<i class="fa fa-info-circle size-icon-filter" aria-hidden="true"></i> ${$helpText.text().trim()} <i class="fa fa-chevron-down size-icon-filter"></i>`);
+    $(document).foundation();
+  }
+
+  bindHelpTextClick();
 });
