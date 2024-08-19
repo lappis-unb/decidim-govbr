fd8021ffed4ad32f1e550a8ed83e6e5504d989b0
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2024-08-19 20:44:43 -0300

    feat: add scopes to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  SCOPE = "decidim.admin".freeze
+  DECIDIM_ADMIN_SCOPE = "decidim.admin".freeze
+  DECIDIM_MEETINGS_SCOPE = "decidim.meetings".freeze
+  DECIDIM_PROPOSALS_SCOPE = "decidim.proposals.admin".freeze
 end
