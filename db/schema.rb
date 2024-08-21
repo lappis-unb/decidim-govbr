f7a4dcb1ffa8d8ad006604fa27944c87673c1374
Gustavo <gustavohenriqueprivado@gmail.com>
2024-08-21 10:50:41 -0300

    feat: adiciona as migrations e a schema com os campos novos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_09_112353) do
+ActiveRecord::Schema.define(version: 2024_08_20_121007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1616,6 +1616,8 @@ ActiveRecord::Schema.define(version: 2024_08_09_112353) do
     t.boolean "show_mobilization"
     t.boolean "is_template", default: false, null: false
     t.jsonb "extra_fields"
+    t.string "mobilization_title"
+    t.integer "mobilization_position"
     t.index ["decidim_area_id"], name: "index_decidim_participatory_processes_on_decidim_area_id"
     t.index ["decidim_organization_id", "slug"], name: "index_unique_process_slug_and_organization", unique: true
     t.index ["decidim_organization_id"], name: "index_decidim_processes_on_decidim_organization_id"
