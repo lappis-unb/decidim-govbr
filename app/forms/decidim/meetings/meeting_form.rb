284d7c9f9aa94ea4791d86cd314899f39960b2f3
Marina <marinamcsouza@gmail.com>
2024-08-31 10:26:21 -0300

    feat: Atualizando merting_form.rb

--- app/forms/decidim/meetings/meeting_form.rb
+++ app/forms/decidim/meetings/meeting_form.rb
@@ -6,6 +6,15 @@ module Decidim
     class MeetingForm < ::Decidim::Meetings::BaseMeetingForm
       include Decidim::AttachmentAttributes
 
+      # Atributos e validações
+      ATTRIBUTES = [
+        :title, :description, :location, :location_hints, :attachment,
+        :decidim_scope_id, :decidim_category_id, :user_group_id,
+        :registration_type, :registrations_enabled, :registration_url,
+        :available_slots, :registration_terms, :iframe_embed_type,
+        :iframe_access_level, :photos, :documents
+      ].freeze
+
       attribute :title, String
       attribute :description, String
       attribute :location, String
@@ -26,24 +35,18 @@ module Decidim
       attachments_attribute :documents
 
       validates :iframe_embed_type, inclusion: { in: Decidim::Meetings::Meeting.participants_iframe_embed_types }
-      validates :title, presence: true
-      validates :description, presence: true
-      validates :type_of_meeting, presence: true
-      validates :location, presence: true, if: ->(form) { form.in_person_meeting? || form.hybrid_meeting? }
-      validates :online_meeting_url, presence: true, url: true, if: ->(form) { form.online_meeting? || form.hybrid_meeting? }
+      validates :title, :description, :type_of_meeting, presence: true
+      validates :location, presence: true, if: -> { in_person_meeting? || hybrid_meeting? }
+      validates :online_meeting_url, presence: true, url: true, if: -> { online_meeting? || hybrid_meeting? }
       validates :registration_type, presence: true
-      validates :available_slots, numericality: { greater_than_or_equal_to: 0 }, presence: true, if: ->(form) { form.on_this_platform? }
-      validates :registration_terms, presence: true, if: ->(form) { form.on_this_platform? }
-      validates :registration_url, presence: true, url: true, if: ->(form) { form.on_different_platform? }
-      validates :category, presence: true, if: ->(form) { form.decidim_category_id.present? }
-      validates :scope, presence: true, if: ->(form) { form.decidim_scope_id.present? }
-      validates :decidim_scope_id, scope_belongs_to_component: true, if: ->(form) { form.decidim_scope_id.present? }
+      validates :available_slots, numericality: { greater_than_or_equal_to: 0 }, presence: true, if: -> { on_this_platform? }
+      validates :registration_terms, presence: true, if: -> { on_this_platform? }
+      validates :registration_url, presence: true, url: true, if: -> { on_different_platform? }
+      validates :category, presence: true, if: -> { decidim_category_id.present? }
+      validates :scope, presence: true, if: -> { decidim_scope_id.present? }
+      validates :decidim_scope_id, scope_belongs_to_component: true, if: -> { decidim_scope_id.present? }
       validates :clean_type_of_meeting, presence: true
-      validates(
-        :iframe_access_level,
-        inclusion: { in: Decidim::Meetings::Meeting.iframe_access_levels },
-        if: ->(form) { %w(embed_in_meeting_page open_in_new_tab).include?(form.iframe_embed_type) }
-      )
+      validates :iframe_access_level, inclusion: { in: Decidim::Meetings::Meeting.iframe_access_levels }, if: -> { %w(embed_in_meeting_page open_in_new_tab).include?(iframe_embed_type) }
       validate :embeddable_meeting_url
 
       delegate :categories, to: :current_component
@@ -61,16 +64,10 @@ module Decidim
 
       alias component current_component
 
-      # Finds the Scope from the given decidim_scope_id, uses the compoenent scope if missing.
-      #
-      # Returns a Decidim::Scope
       def scope
-        @scope ||= @attributes["decidim_scope_id"].value ? current_component.scopes.find_by(id: @attributes["decidim_scope_id"].value) : current_component.scope
+        @scope ||= find_scope
       end
 
-      # Scope identifier
-      #
-      # Returns the scope identifier related to the meeting
       def decidim_scope_id
         super || scope&.id
       end
@@ -85,33 +82,24 @@ module Decidim
         type_of_meeting.presence
       end
 
+      # Métodos de Seleção
       def type_of_meeting_select
-        Decidim::Meetings::Meeting::TYPE_OF_MEETING.map do |type|
-          [
-            I18n.t("type_of_meeting.#{type}", scope: "decidim.meetings"),
-            type
-          ]
-        end
+        select_options(Decidim::Meetings::Meeting::TYPE_OF_MEETING, "type_of_meeting")
       end
 
       def iframe_access_level_select
-        Decidim::Meetings::Meeting.iframe_access_levels.map do |level, _value|
-          [
-            I18n.t("iframe_access_level.#{level}", scope: "decidim.meetings"),
-            level
-          ]
-        end
+        select_options(Decidim::Meetings::Meeting.iframe_access_levels.keys, "iframe_access_level")
       end
 
       def iframe_embed_type_select
-        Decidim::Meetings::Meeting.participants_iframe_embed_types.map do |type, _value|
-          [
-            I18n.t("iframe_embed_type.#{type}", scope: "decidim.meetings"),
-            type
-          ]
-        end
+        select_options(Decidim::Meetings::Meeting.participants_iframe_embed_types.keys, "iframe_embed_type")
       end
 
+      def registration_type_select
+        select_options(Decidim::Meetings::Meeting::REGISTRATION_TYPE, "registration_type")
+      end
+
+      # Métodos de Verificação
       def on_this_platform?
         registration_type == "on_this_platform"
       end
@@ -120,23 +108,34 @@ module Decidim
         registration_type == "on_different_platform"
       end
 
-      def registration_type_select
-        Decidim::Meetings::Meeting::REGISTRATION_TYPE.map do |type|
-          [
-            I18n.t("registration_type.#{type}", scope: "decidim.meetings"),
-            type
-          ]
-        end
-      end
-
       def registrations_enabled
         on_this_platform?
       end
 
+      # Método de URL Embutível
       def embeddable_meeting_url
-        if online_meeting_url.present? && %w(embed_in_meeting_page open_in_live_event_page).include?(iframe_embed_type)
-          embedder_service = Decidim::Meetings::MeetingIframeEmbedder.new(online_meeting_url)
-          errors.add(:iframe_embed_type, :not_embeddable) unless embedder_service.embeddable?
+        return unless online_meeting_url.present? && %w(embed_in_meeting_page open_in_live_event_page).include?(iframe_embed_type)
+
+        embedder_service = Decidim::Meetings::MeetingIframeEmbedder.new(online_meeting_url)
+        errors.add(:iframe_embed_type, :not_embeddable) unless embedder_service.embeddable?
+      end
+
+      private
+
+      def select_options(options, scope)
+        options.map do |option|
+          [
+            I18n.t("#{scope}.#{option}", scope: "decidim.meetings"),
+            option
+          ]
+        end
+      end
+
+      def find_scope
+        if @attributes["decidim_scope_id"].value
+          current_component.scopes.find_by(id: @attributes["decidim_scope_id"].value)
+        else
+          current_component.scope
         end
       end
     end
