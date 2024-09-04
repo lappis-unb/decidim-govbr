03358d0243c9778b32abecaaeab0351e7fc4713f
Gustavo <gustavohenriqueprivado@gmail.com>
2024-09-04 11:35:09 -0300

    feat: adicionando commands que adicionam badges

--- app/commands/decidim/admin/update_one_proposal_badge.rb
+++ app/commands/decidim/admin/update_one_proposal_badge.rb
@@ -3,16 +3,18 @@
 module Decidim
   module Admin
     # This command gets called when a component is created from the admin panel.
-    class UpdateBadgeProposals < Decidim::Command
+    class UpdateOneProposalBadge < Decidim::Command
       attr_reader :current_component, :user
 
       # Public: Initializes the command.
       #
       # form    - The form from which the data in this component comes from.
       # proposal - The proposal to update.
-      def initialize(current_component, user)
+      def initialize(current_component, proposal_id, badge_name, user)
         @component = current_component
         @user = user
+        @proposal_id = proposal_id
+        @badge_name = badge_name
       end
 
       # Public: Creates the Component.
@@ -21,7 +23,7 @@ module Decidim
       def call
         Decidim.traceability.perform_action!("update", @component, @user) do
           transaction do
-            insert_proposal_most_voted_label
+            update_proposal_badge
           end
         end
         broadcast(:ok)
@@ -29,28 +31,14 @@ module Decidim
 
       private
 
-      def insert_proposal_most_voted(proposal)
+      def update_proposal_badge
+        proposal = Decidim::Proposals::Proposal.find(@proposal_id)
         badge_array = proposal.badge_array
 
-        if badge_array.exclude?("Mais Votada")
-          badge_array << "Mais Votada"
+        if badge_array.exclude?(@badge_name)
+          badge_array << @badge_name
           proposal.update(badge_array: badge_array)
         end
-      end
-
-      def insert_proposal_most_voted_label
-        ten_most_voted_proposals.each do |proposal|
-          insert_proposal_most_voted(proposal)
-        end
-      end
-
-      def reorder_by_votes
-        proposals = Decidim::Proposals::Proposal.where(component: @component)
-        proposals.order(proposal_votes_count: :desc)
-      end
-
-      def ten_most_voted_proposals
-        reorder_by_votes.limit(@component.settings.most_voted_rule) if @component.settings.most_voted_rule.positive?
       end
     end
   end
