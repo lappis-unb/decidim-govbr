4c5fbce0c6e9dc24ac409dd7dacf6b5467a3cb62
Arthurlima544 <arthurlindaolima544@gmail.com>
2024-09-08 16:21:24 -0300

    removing enforce_permission_to, for better debug by the development team, and refact other functions that is aplicable to functions set_flash_and_redirect and handle_validation_failures

--- app/controllers/decidim/proposals/admin/participatory_texts_controller.rb
+++ app/controllers/decidim/proposals/admin/participatory_texts_controller.rb
@@ -7,7 +7,6 @@ module Decidim
       class ParticipatoryTextsController < Admin::ApplicationController
         helper_method :proposal
         helper ParticipatoryTextsHelper
-        before_action -> { enforce_permission_to :manage, :participatory_texts }, only: %i[index new_import import update update_as_preview edit_as_preview discard]
 
         def index
           @drafts = Proposal.where(component: current_component).drafts.order(:position)
@@ -16,6 +15,7 @@ module Decidim
         end
 
         def new_import
+          enforce_permission_to :manage, :participatory_texts
           participatory_text = Decidim::Proposals::ParticipatoryText.find_by(component: current_component)
           @import = form(Admin::ImportParticipatoryTextForm).from_model(participatory_text)
         end
@@ -33,6 +33,7 @@ module Decidim
         end
 
         def import
+          enforce_permission_to :manage, :participatory_texts
           @import = form(Admin::ImportParticipatoryTextForm).from_params(params)
 
           Admin::ImportParticipatoryText.call(@import) do
@@ -55,14 +56,15 @@ module Decidim
         # When `save_draft` param exists, proposals are only saved.
         # When no `save_draft` param is set, proposals are saved and published.
         def update
+          enforce_permission_to :manage, :participatory_texts
+
           form_params = params.require(:preview_participatory_text).permit!
           @preview_form = form(Admin::PreviewParticipatoryTextForm).from_params(proposals: form_params[:proposals_attributes]&.values, proposal_to_add: form_params[:proposal_to_add])
 
           if params[:save_draft].present?
             UpdateParticipatoryText.call(@preview_form) do
               on(:ok) do
-                flash[:notice] = I18n.t("participatory_texts.update.success", scope: "decidim.proposals.admin")
-                redirect_to EngineRouter.admin_proxy(current_component).participatory_texts_path
+                set_flash_and_redirect("participatory_texts.update.success", EngineRouter.admin_proxy(current_component).participatory_texts_path)
               end
 
               on(:invalid) do |failures|
@@ -72,33 +74,28 @@ module Decidim
           else
             PublishParticipatoryText.call(@preview_form) do
               on(:ok) do
-                flash[:notice] = I18n.t("participatory_texts.publish.success", scope: "decidim.proposals.admin")
-                redirect_to proposals_path
+                set_flash_and_redirect("participatory_texts.publish.success", proposals_path)
               end
 
               on(:invalid) do |failures|
-                alert_msg = [I18n.t("participatory_texts.publish.invalid", scope: "decidim.proposals.admin")]
-                failures.each_pair { |id, msg| alert_msg << "ID:[#{id}] #{msg}" }
-                flash.now[:alert] = alert_msg.join("<br/>").html_safe
-                index
-                render action: "index"
+                handle_validation_failures(failures, "index")
               end
             end
           end
         end
 
         def update_as_preview
+          enforce_permission_to :manage, :participatory_texts
           form_params = params.require(:preview_participatory_text).permit!
 
           @preview_form = form(Admin::PreviewParticipatoryTextForm).from_params(proposals: form_params[:proposals_attributes]&.values, proposal_to_add: form_params[:proposal_to_add])
 
           PublishParticipatoryText.call(@preview_form) do
             on(:ok) do
-              flash[:notice] = I18n.t("participatory_texts.update.success", scope: "decidim.proposals.admin")
               if form_params[:proposal_to_add].present?
-                redirect_to EngineRouter.admin_proxy(current_component).edit_as_preview_participatory_texts_path
+                set_flash_and_redirect("participatory_texts.update.success", EngineRouter.admin_proxy(current_component).edit_as_preview_participatory_texts_path)
               else
-                redirect_to proposals_path
+                set_flash_and_redirect("participatory_texts.update.success", proposals_path)
               end
             end
 
@@ -112,6 +109,8 @@ module Decidim
         end
 
         def edit_as_preview
+          enforce_permission_to :manage, :participatory_texts
+
           @participatory_texts = Proposal.where(component: current_component).order(:position)
 
           @preview_form = form(Admin::PreviewParticipatoryTextForm).instance
@@ -120,10 +119,11 @@ module Decidim
 
         # Removes all the unpublished proposals (drafts).
         def discard
+          enforce_permission_to :manage, :participatory_texts
+
           DiscardParticipatoryText.call(current_component) do
             on(:ok) do
-              flash[:notice] = I18n.t("participatory_texts.discard.success", scope: "decidim.proposals.admin")
-              redirect_to EngineRouter.admin_proxy(current_component).participatory_texts_path
+              set_flash_and_redirect("participatory_texts.discard.success", EngineRouter.admin_proxy(current_component).participatory_texts_path)
             end
           end
         end
