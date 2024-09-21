6ac73525a737a7cce21c703603d3816d9b0b4852
Eduardo Nunes <eduardonunes2525@gmail.com>
2024-09-21 11:29:02 -0300

    feat: adiciona mascara no reenvio dos documentos

--- db/schema.rb
+++ db/schema.rb
@@ -2060,7 +2060,7 @@ ActiveRecord::Schema.define(version: 2024_08_30_024117) do
     t.text "about"
     t.datetime "accepted_tos_version"
     t.string "newsletter_token", default: ""
-    t.datetime "newsletter_notifications_at", default: "2024-07-02 17:35:34"
+    t.datetime "newsletter_notifications_at", default: "2024-06-24 13:13:37"
     t.string "type", null: false
     t.jsonb "extended_data", default: {}
     t.integer "following_count", default: 0, null: false
